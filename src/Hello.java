5a88894c3b8351152f294057dc8dc5551bde5146
James Cowgill <james410@cowgill.org.uk>
2013-06-10 17:41:40 +0100

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+
+/**
+ * Prints hello world
+ *
+ * @author James
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
