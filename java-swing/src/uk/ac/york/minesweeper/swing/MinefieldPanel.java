ba0849f4f15a5725ec9d303c9c452a70693e5a15
James Cowgill <james410@cowgill.org.uk>
2013-07-15 23:27:43 +0100

    Abstracted the drawing system into a MinefieldDrawer class
    - This means that most of the drawing has now got nothing to do
      with swing and so it can be used on android as well

--- java-swing/src/uk/ac/york/minesweeper/swing/MinefieldPanel.java
+++ java-swing/src/uk/ac/york/minesweeper/swing/MinefieldPanel.java
@@ -5,11 +5,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import javax.swing.JComponent;
@@ -17,6 +17,7 @@ import javax.swing.SwingUtilities;
 
 import uk.ac.york.minesweeper.GameState;
 import uk.ac.york.minesweeper.Minefield;
+import uk.ac.york.minesweeper.MinefieldDrawer;
 import uk.ac.york.minesweeper.TileState;
 
 /**
@@ -29,41 +30,11 @@ public class MinefieldPanel extends JComponent
     /** Size of all the tiles */
     private static final int TILE_SIZE = 32;
 
-    /** Width of the bevel */
-    private static final int BEVEL_WIDTH = 2;
-
     /** Font vertical offset (from top to BASELINE) */
     private static final int FONT_VOFFSET = 24;
 
     /** The font to draw numbers with */
     private static final Font FONT = new Font(Font.MONOSPACED, Font.BOLD, 24);
-
-
-    /** Default background colour */
-    private static final Color COLOUR_BACKGROUND = new Color(0xC0, 0xC0, 0xC0);
-
-    /** Light grey for bevels */
-    private static final Color COLOUR_LIGHT = new Color(0xE0, 0xE0, 0xE0);
-
-    /** Dark grey for bevels */
-    private static final Color COLOUR_DARK = new Color(0x80, 0x80, 0x80);
-
-    /** Colour of question marks */
-    private static final Color COLOUR_QUESTION = Color.WHITE;
-
-    /** The colours of the numbers (0 is unused) */
-    private static final Color[] COLOUR_NUMBERS = new Color[]
-    {
-        null,                           // 0 = Unused
-        new Color(0x00, 0x00, 0xFF),    // 1 = Blue
-        new Color(0x00, 0x7F, 0x00),    // 2 = Green
-        new Color(0xFF, 0x00, 0x00),    // 3 = Red
-        new Color(0x2F, 0x2F, 0x9F),    // 4 = Dark Blue
-        new Color(0x7F, 0x00, 0x00),    // 5 = Maroon
-        new Color(0x9F, 0x9F, 0x2F),    // 6 = Turquoise
-        new Color(0x00, 0x00, 0x00),    // 7 = Black
-        new Color(0x7F, 0x7F, 0x7F),    // 8 = Grey
-    };
 
     /** Current minefield */
     private Minefield minefield;
@@ -73,6 +44,9 @@ public class MinefieldPanel extends JComponent
 
     /** List of state change listeners */
     private ArrayList<MinefieldStateChangeListener> listeners = new ArrayList<MinefieldStateChangeListener>();
+
+    /** Class which draws the game area */
+    private SwingDrawer drawer = new SwingDrawer();
 
     /**
      * Initializes a new MinefieldPanel with the given Minefield
@@ -84,7 +58,6 @@ public class MinefieldPanel extends JComponent
     public MinefieldPanel(Minefield minefield)
     {
         this.addMouseListener(new MouseEventListener());
-        this.setBackground(COLOUR_BACKGROUND);
         this.setOpaque(true);
         this.setFont(FONT);
         this.setMinefield(minefield);
@@ -155,117 +128,10 @@ public class MinefieldPanel extends JComponent
         this.fireStateChangeEvent();
     }
 
-    /**
-     * Draws a character on a tile
-     *
-     * @param g graphics object
-     * @param x x position of top-left of tile
-     * @param y y position of top-left of tile
-     * @param c character to draw
-     */
-    private static void drawCharacter(Graphics g, int x, int y, char c)
-    {
-        // Get coordinates to draw at
-        int drawX = x + (TILE_SIZE - g.getFontMetrics().charWidth(c)) / 2;
-        int drawY = y + FONT_VOFFSET;
-
-        // Draw the character
-        g.drawChars(new char[] { c }, 0, 1, drawX, drawY);
-    }
-
-    /**
-     * Draws an image at the given tile location
-     *
-     * @param g graphics object
-     * @param x x position of top-left of tile
-     * @param y y position of top-left of tile
-     * @param img image to draw
-     */
-    private static void drawImage(Graphics g, int tileX, int tileY, BufferedImage img)
-    {
-        int xOff = tileX + (TILE_SIZE - img.getWidth()) / 2;
-        int yOff = tileY + (TILE_SIZE - img.getHeight()) / 2;
-
-        g.drawImage(img, xOff, yOff, null);
-    }
-
     @Override
-    public void paintComponent(Graphics gOld)
+    public void paintComponent(Graphics g)
     {
-        Graphics2D g = (Graphics2D) gOld;
-
-        // Get selected tile position
-        int selectedX = (selectedTile == null ? -1 : selectedTile.x);
-        int selectedY = (selectedTile == null ? -1 : selectedTile.y);
-
-        // Make the numbers look a little nicer
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Draw background
-        if (isOpaque())
-        {
-            g.setColor(getBackground());
-            g.fillRect(0, 0, getWidth(), getHeight());
-        }
-
-        // Draw all the tiles
-        for (int x = 0; x < minefield.getWidth(); x++)
-        {
-            for (int y = 0; y < minefield.getHeight(); y++)
-            {
-                int graphicsX1 = x * TILE_SIZE;
-                int graphicsY1 = y * TILE_SIZE;
-
-                // Draw standard background
-                g.setColor(COLOUR_DARK);
-                g.drawLine(graphicsX1, graphicsY1, graphicsX1 + TILE_SIZE, graphicsY1);
-                g.drawLine(graphicsX1, graphicsY1, graphicsX1, graphicsY1 + TILE_SIZE);
-
-                // Covered or uncovered?
-                if (minefield.getTileState(x, y) == TileState.UNCOVERED)
-                {
-                    // Draw the correct symbol
-                    int tileValue = minefield.getTileValue(x, y);
-
-                    if (tileValue < 0)
-                    {
-                        drawImage(g, graphicsX1, graphicsY1, Images.MINE);
-                    }
-                    else if (tileValue > 0)
-                    {
-                        g.setColor(COLOUR_NUMBERS[tileValue]);
-                        drawCharacter(g, graphicsX1, graphicsY1, (char) ('0' + tileValue));
-                    }
-                }
-                else
-                {
-                    // Only draw the bevel background if this is NOT the selected tile
-                    if (x != selectedX || y != selectedY)
-                    {
-                        int bevelX2 = graphicsX1 + TILE_SIZE - BEVEL_WIDTH;
-                        int bevelY2 = graphicsY1 + TILE_SIZE - BEVEL_WIDTH;
-
-                        g.setColor(COLOUR_LIGHT);
-                        g.fillRect(graphicsX1, graphicsY1, TILE_SIZE, BEVEL_WIDTH);
-                        g.fillRect(graphicsX1, graphicsY1, BEVEL_WIDTH, TILE_SIZE);
-                        g.setColor(COLOUR_DARK);
-                        g.fillRect(graphicsX1, bevelY2,    TILE_SIZE, BEVEL_WIDTH);
-                        g.fillRect(bevelX2,    graphicsY1, BEVEL_WIDTH, TILE_SIZE);
-                    }
-
-                    // Draw flag or question mark if needed
-                    if (minefield.getTileState(x, y) == TileState.FLAGGED)
-                    {
-                        drawImage(g, graphicsX1, graphicsY1, Images.FLAG);
-                    }
-                    else if (minefield.getTileState(x, y) == TileState.QUESTION)
-                    {
-                        g.setColor(COLOUR_QUESTION);
-                        drawCharacter(g, graphicsX1, graphicsY1, '?');
-                    }
-                }
-            }
-        }
+        drawer.draw((Graphics2D) g);
     }
 
     @Override
@@ -381,6 +247,98 @@ public class MinefieldPanel extends JComponent
                 selectedTile = null;
                 repaint();
             }
+        }
+    }
+
+    /**
+     * The class which actually draws the game
+     */
+    private class SwingDrawer extends MinefieldDrawer<Graphics2D, Color>
+    {
+        @Override
+        protected float getTileSize()
+        {
+            return TILE_SIZE;
+        }
+
+        @Override
+        protected int getSelectedX()
+        {
+            return (selectedTile == null ? -1 : selectedTile.x);
+        }
+
+        @Override
+        protected int getSelectedY()
+        {
+            return (selectedTile == null ? -1 : selectedTile.y);
+        }
+
+        @Override
+        protected Minefield getMinefield()
+        {
+            return minefield;
+        }
+
+        @Override
+        protected Color createPaint(int color, boolean forText)
+        {
+            return new Color(color);
+        }
+
+        @Override
+        protected void resetDrawArea(Color background)
+        {
+            Graphics2D g = getDrawContext();
+
+            // Make the numbers look a little nicer
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // Draw background
+            if (isOpaque())
+            {
+                g.setColor(background);
+                g.fillRect(0, 0, getWidth(), getHeight());
+            }
+        }
+
+        @Override
+        protected void fillRect(float x, float y, float width, float height, Color paint)
+        {
+            Graphics2D g = getDrawContext();
+
+            g.setColor(paint);
+            g.fillRect((int) x, (int) y, (int) width, (int) height);
+        }
+
+        @Override
+        protected void drawCharacter(float x, float y, char c, Color paint)
+        {
+            Graphics2D g = getDrawContext();
+
+            // Get coordinates to draw at
+            int drawX = (int) (x + (TILE_SIZE - g.getFontMetrics().charWidth(c)) / 2);
+            int drawY = (int) (y + FONT_VOFFSET);
+
+            // Draw the character
+            g.setColor(paint);
+            g.drawChars(new char[] { c }, 0, 1, drawX, drawY);
+        }
+
+        @Override
+        protected void drawMine(float x, float y, float size)
+        {
+            drawImage(x, y, size, Images.MINE);
+        }
+
+        @Override
+        protected void drawFlag(float x, float y, float size)
+        {
+            drawImage(x, y, size, Images.FLAG);
+        }
+
+        private void drawImage(float x, float y, float size, Image img)
+        {
+            getDrawContext().drawImage(img, (int) x, (int) y, (int) size, (int) size, null);
         }
     }
 }
---
+++ shared-src/uk/ac/york/minesweeper/MinefieldDrawer.java
@@ -0,0 +1,263 @@
+package uk.ac.york.minesweeper;
+
+/**
+ * Helper class use to draw the minefield game area
+ *
+ * @param <TContext> the type of context used in the drawer
+ * @param <TPaint> the type of cached paints
+ */
+public abstract class MinefieldDrawer<TContext, TPaint>
+{
+    /** Number to divide tile size by to get bevel width */
+    private static final float BEVEL_WIDTH_DIVISOR = 16f;
+
+    /** Number to divide tile size by to get line width */
+    private static final float LINE_WIDTH_DIVISOR = 32f;
+
+    /** Number to multiply tile size by to get image sizes */
+    private static final float IMAGE_SIZE_MULTIPLIER = 0.75f;
+
+    // All colours are RGB ints (0xRRGGBB)
+
+    /** Default background colour */
+    private static final int COLOUR_BACKGROUND = 0xC0C0C0;
+
+    /** Light grey for bevels */
+    private static final int COLOUR_LIGHT = 0xE0E0E0;
+
+    /** Dark grey for bevels */
+    private static final int COLOUR_DARK = 0x808080;
+
+    /** Colour of question marks */
+    private static final int COLOUR_QUESTION = 0xFFFFFF;
+
+    /** The colours of the numbers (0 is unused) */
+    private static final int[] COLOUR_NUMBERS = new int[]
+    {
+        0,          // 0 = Unused
+        0x0000FF,   // 1 = Blue
+        0x007F00,   // 2 = Green
+        0xFF0000,   // 3 = Red
+        0x2F2F9F,   // 4 = Dark Blue
+        0x7F0000,   // 5 = Maroon
+        0x9F9F2F,   // 6 = Turquoise
+        0x000000,   // 7 = Black
+        0x7F7F7F,   // 8 = Grey
+    };
+
+    // Cached colours
+    private boolean doneInit;
+
+    private TPaint paintBackground;
+    private TPaint paintLight;
+    private TPaint paintDark;
+    private TPaint paintQuestion;
+    private TPaint[] paintNumbers;
+
+    /** The saved drawer context */
+    private TContext context;
+
+    /**
+     * Gets this drawer's context
+     *
+     * This field is only value during the execution of the draw method
+     *
+     * @return the context
+     */
+    public TContext getDrawContext()
+    {
+        return context;
+    }
+
+    /**
+     * Initializes the cached colours
+     */
+    public void init()
+    {
+        if (!doneInit)
+        {
+            paintBackground  = createPaint(COLOUR_BACKGROUND, false);
+            paintDark        = createPaint(COLOUR_DARK,       false);
+            paintLight       = createPaint(COLOUR_LIGHT,      false);
+            paintQuestion    = createPaint(COLOUR_QUESTION,   true);
+
+            @SuppressWarnings("unchecked")
+            TPaint[] locNumbers = (TPaint[]) new Object[COLOUR_NUMBERS.length];
+
+            for (int i = 0; i < COLOUR_NUMBERS.length; i++)
+                locNumbers[i] = createPaint(COLOUR_NUMBERS[i], true);
+
+            paintNumbers = locNumbers;
+
+            doneInit = true;
+        }
+    }
+
+    /**
+     * Draws the minefield using the given drawing context
+     *
+     * @param drawContext context to draw to
+     */
+    public void draw(TContext drawContext)
+    {
+        // Ensure paints have been generated
+        init();
+
+        // Save context
+        this.context = drawContext;
+
+        // Get drawer parameters
+        Minefield minefield = getMinefield();
+        int selectedX = getSelectedX();
+        int selectedY = getSelectedY();
+
+        float tileSize = getTileSize();
+        float bevelWidth = tileSize / BEVEL_WIDTH_DIVISOR;
+        float lineWidth = tileSize / LINE_WIDTH_DIVISOR;
+        float imageSize = tileSize * IMAGE_SIZE_MULTIPLIER;
+        float imageOffset = tileSize * ((1 - IMAGE_SIZE_MULTIPLIER) / 2f);
+
+        // Clear the screen
+        resetDrawArea(paintBackground);
+
+        // Draw all the tiles
+        for (int x = 0; x < minefield.getWidth(); x++)
+        {
+            for (int y = 0; y < minefield.getHeight(); y++)
+            {
+                float graphicsX1 = x * tileSize;
+                float graphicsY1 = y * tileSize;
+
+                // Draw standard background
+                fillRect(graphicsX1, graphicsY1, tileSize, lineWidth, paintDark);
+                fillRect(graphicsX1, graphicsY1, lineWidth, tileSize, paintDark);
+
+                // Covered or uncovered?
+                if (minefield.getTileState(x, y) == TileState.UNCOVERED)
+                {
+                    // Draw the correct symbol
+                    int tileValue = minefield.getTileValue(x, y);
+
+                    if (tileValue < 0)
+                    {
+                        drawMine(graphicsX1 + imageOffset, graphicsY1 + imageOffset, imageSize);
+                    }
+                    else if (tileValue > 0)
+                    {
+                        drawCharacter(graphicsX1, graphicsY1,
+                                (char) ('0' + tileValue), paintNumbers[tileValue]);
+                    }
+                }
+                else
+                {
+                    // Only draw the bevel background if this is NOT the selected tile
+                    if (x != selectedX || y != selectedY)
+                    {
+                        float bevelX2 = graphicsX1 + tileSize - bevelWidth;
+                        float bevelY2 = graphicsY1 + tileSize - bevelWidth;
+
+                        fillRect(graphicsX1, graphicsY1, tileSize, bevelWidth, paintLight);
+                        fillRect(graphicsX1, graphicsY1, bevelWidth, tileSize, paintLight);
+                        fillRect(graphicsX1, bevelY2,    tileSize, bevelWidth, paintDark);
+                        fillRect(bevelX2,    graphicsY1, bevelWidth, tileSize, paintDark);
+                    }
+
+                    // Draw flag or question mark if needed
+                    if (minefield.getTileState(x, y) == TileState.FLAGGED)
+                    {
+                        drawFlag(graphicsX1 + imageOffset, graphicsY1 + imageOffset, imageSize);
+                    }
+                    else if (minefield.getTileState(x, y) == TileState.QUESTION)
+                    {
+                        drawCharacter(graphicsX1, graphicsY1, '?', paintQuestion);
+                    }
+                }
+            }
+        }
+
+        // Clear context
+        this.context = null;
+    }
+
+    /**
+     * Returns the size of tiles
+     *
+     * @return the tile size
+     */
+    protected abstract float getTileSize();
+
+    /**
+     * Returns the selected tile's x position or -1 if no tile is selected
+     *
+     * @return selected tile x position
+     */
+    protected abstract int getSelectedX();
+
+    /**
+     * Returns the selected tile's y position or -1 if no tile is selected
+     *
+     * @return selected tile y position
+     */
+    protected abstract int getSelectedY();
+
+    /**
+     * Returns the minefield to draw
+     *
+     * @return minefield to draw
+     */
+    protected abstract Minefield getMinefield();
+
+    /**
+     * Creates a paint object for the given RGB colour
+     *
+     * @param color colour to create object for
+     * @param forText true if the colour will only be used for text, false for rectangles
+     */
+    protected abstract TPaint createPaint(int color, boolean forText);
+
+    /**
+     * Clears the draw area to the given colour and prepares for drawing
+     *
+     * @param background background colour to draw
+     */
+    protected abstract void resetDrawArea(TPaint background);
+
+    /**
+     * Draws a filled in rectangle at the given location using the given paint
+     *
+     * @param x x position of top-left corner
+     * @param y y position of top-left corner
+     * @param width width of the rectangle
+     * @param height height of the rectangle
+     * @param paint paint to draw the rectangle with
+     */
+    protected abstract void fillRect(float x, float y, float width, float height, TPaint paint);
+
+    /**
+     * Draws a single character at the given tile position using the given colour
+     *
+     * @param x x position of the top-left of the tile
+     * @param y y position of the top-left of the tile
+     * @param c character to draw
+     * @param paint paint to draw with
+     */
+    protected abstract void drawCharacter(float x, float y, char c, TPaint paint);
+
+    /**
+     * Draws a mine at the given location
+     *
+     * @param x x position of top-left corner
+     * @param y y position of top-left corner
+     * @param size width and height of the mine
+     */
+    protected abstract void drawMine(float x, float y, float size);
+
+    /**
+     * Draws a flag at the given location
+     *
+     * @param x x position of top-left corner
+     * @param y y position of top-left corner
+     * @param size width and height of the flag
+     */
+    protected abstract void drawFlag(float x, float y, float size);
+}
